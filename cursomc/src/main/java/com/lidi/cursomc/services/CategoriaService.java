7c1f965f3f4634ba834d4704fefea9b9802c9119
Lidiane <lidianegomesbarbosa@gmail.com>
2018-11-16 13:48:52 -0300

    Criando Repository e Service para Categoria

---
+++ cursomc/src/main/java/com/lidi/cursomc/services/CategoriaService.java
@@ -0,0 +1,19 @@
+package com.lidi.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lidi.cursomc.domain.Categoria;
+import com.lidi.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	public Categoria buscar(Integer id) {
+		Categoria obj = categoriaRepository.findOne(id);
+		return obj;
+	}
+
+}
