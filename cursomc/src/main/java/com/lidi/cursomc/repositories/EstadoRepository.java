58f357cbe5e477c3ba95a83564ddcad52370f9bc
Lidiane <lidianegomesbarbosa@gmail.com>
2018-11-20 09:32:16 -0300

    Estado e cidade

---
+++ cursomc/src/main/java/com/lidi/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.lidi.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lidi.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+	
+}
