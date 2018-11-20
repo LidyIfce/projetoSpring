5875846b876f12d642cc3e9ba03ae98a1edd0597
Lidiane <lidianegomesbarbosa@gmail.com>
2018-11-20 08:57:36 -0300

    Ajustes no endpoind/categorias{id}

--- cursomc/src/main/java/com/lidi/cursomc/domain/Produto.java
+++ cursomc/src/main/java/com/lidi/cursomc/domain/Produto.java
@@ -12,6 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class Produto implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -21,7 +23,8 @@ public class Produto implements Serializable{
 	private Integer id;
 	private String nome;
 	private double preco;
-	
+
+	@JsonIgnore
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA",
 			joinColumns = @JoinColumn(name = "produto_id"),
